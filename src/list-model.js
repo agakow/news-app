81145d1367ae1097c0faf57972dabc2ee0c736ce
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-08-22 11:04:31 +0100

    refactoring

--- src/list-model.js
+++ src/list-model.js
@@ -2,36 +2,35 @@ var List = (function () {
 
   var articles;
 
-  function get(url, sectionName) {
+  function get(type, url) {
       articles = "";
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             var data = JSON.parse(xhttp.responseText);
-            if(url==="headlines") {
+            if(type==="headlines") {
               articles = data.response.editorsPicks;
             } else {
               articles = data.response.results;
             }
           }
         };
-      if(url==="headlines") {
-      xhttp.open("GET", 'http://content.guardianapis.com/uk?show-editors-picks=true&show-fields=thumbnail,body&api-key=' + Guardian.key());
-    } else {
-      xhttp.open("GET", 'http://content.guardianapis.com/search?section=' + sectionName + '&show-fields=thumbnail,body&api-key=' + Guardian.key());
-    }
+
+      xhttp.open("GET", url + Guardian.key());
       xhttp.send();
     }
 
   return {
 
     headlines: function() {
-        get("headlines");
+        url = 'http://content.guardianapis.com/uk?show-editors-picks=true&show-fields=thumbnail,body&api-key=';
+        get("headlines", url);
       },
 
     section: function(sectionName) {
-        get("section", sectionName);
-        },
+      url = 'http://content.guardianapis.com/search?section=' + sectionName + '&show-fields=thumbnail,body&api-key=';
+        get("section", url);
+      },
 
     printArticles: function() {
       return articles;
